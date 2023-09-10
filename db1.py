46f96a24378a88af6102004541734d0c6f2379e9
Kusakra <144541153+Kusakra@users.noreply.github.com>
2023-09-10 20:43:12 +0800

    Add files via upload

--- db1.py
+++ db1.py
@@ -1,33 +1,41 @@
+#简易可存储键值对数据库
 import json
 db={}
 def put(key,value):   
     if db.get(key,None)!=None:
-        str=input("该键值对已存在！是否覆盖原有的值？Y/N")
+        str=input("该键值对已存在！是否覆盖原有的值？Y/N\n")
         if str=='Y'or'y':
             db[key]=value
         elif str=="N"or"n":
             pass
-        else :print("请输入Y/N！")
+        else :print("请输入Y/N！\n")
     elif db.get(key,None)==None:
             db[key]=value
             print ("OK")
     else :pass
 def main():
-    ori=input()
+    ori=input("请输入命令：\n")
     ori=ori.split()
-    if "put" in ori:
+    if "help" in ori:           #help
+        print("输入指令：put <key> <value>")
+        print("查询指令：get <key>")
+        print("删除指令：del <key>")
+        print("列出所有键值对：getall")
+    elif "getall" in ori:
+        print(db)
+    elif "put" in ori:          #put
         if len(ori)!=3:
             print("格式错误！")     
         else:     
             put(ori[1],ori[2])
-    elif "get" in ori:
+    elif "get" in ori:          #get
         if len(ori)!=2:
             print ("格式错误！")
         else:    
             if ori[1] in db:
                 print ("key:"+ori[1]+" "+"value:"+db["{}".format(ori[1])])
             else :print ("NOT_FOUND")
-    elif "del" in ori :
+    elif "del" in ori:         #del
         if len(ori)!=2:
             print ("格式错误！")
         else:
@@ -35,8 +43,9 @@ def main():
                 del db["{}".format(ori[1])]
                 print ("OK")
             else : print("输入的键无效！")
-    else : print("格式错误！")
-try:
+    else : print("格式错误或输入了未知指令，请再次输入！")
+
+try:                #open the file
     f=open('db.json','r')
     data=json.load(f)
     db=data
@@ -44,12 +53,13 @@ try:
     f=open('db.json','w+')
 except FileNotFoundError:
     f=open('db.json','w+')
-judge=1
-while judge==1:
+judge=1             #main program start     
+print("输入/help以查询指令")
+while judge==1:     
     main()
     data=json.dumps(db)
-    judge=input("按1继续执行，按其他键退出")
-    try:
+    judge=input("按1继续执行，按其他键退出\n")
+    try:    
         a=float(judge)
     except ValueError:
         f.write(data)
